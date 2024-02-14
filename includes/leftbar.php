89fdc5fb3b924737acfd6bf036f26d005d4acd5d
VishalSingh3026 <121923704+VishalSingh3026@users.noreply.github.com>
2024-02-14 10:52:01 +0530

    Add files via upload

---
+++ includes/leftbar.php
@@ -0,0 +1,59 @@
+<div class="left-sidebar bg-black-300 box-shadow ">
+                        <div class="sidebar-content">
+                            <div class="user-info closed">
+                                <img src="http://placehold.it/90/c2c2c2?text=User" alt="John Doe" class="img-circle profile-img">
+                                <h6 class="title">Admin</h6>
+                                <small class="info">Administrator</small>
+                            </div>
+                            <!-- /.user-info -->
+
+                            <div class="sidebar-nav">
+                                <ul class="side-nav color-gray">
+                                    <li class="nav-header">
+                                        <span class="">Main Category</span>
+                                    </li>
+                                    <li>
+                                        <a href="dashboard.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span> </a>
+                                     
+                                    </li>
+
+                                    <li class="nav-header">
+                                        <span class="">Appearance</span>
+                                    </li>
+                                    <li class="has-children">
+                                        <a href="#"><i class="fa fa-file-text"></i> <span>CROWD CONTROL</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="create-class.php"><i class="fa fa-bars"></i> <span>Update Crowd Info</span></a></li>
+                                           
+                                        </ul>
+                                    </li>
+  <li class="has-children">
+                                        <ul class="child-nav">
+                                           
+                                        </ul>
+                                    </li>
+   <li class="has-children">
+                                        <ul class="child-nav">
+                                           
+                                        </ul>
+                                    </li>
+<li class="has-children">
+                                        <ul class="child-nav">
+                                           
+                                        </ul>        </li>
+
+
+<li class="has-children">
+                                        <ul class="child-nav">
+                                           
+                                        </ul>        </li>
+
+
+
+                                           
+                            
+                            </div>
+                            <!-- /.sidebar-nav -->
+                        </div>
+                        <!-- /.sidebar-content -->
+                    </div>
